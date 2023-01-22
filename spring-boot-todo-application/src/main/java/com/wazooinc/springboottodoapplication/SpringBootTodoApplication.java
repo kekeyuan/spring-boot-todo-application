3ae0316aaac34867dbd943afc0046ed059b9501b
kekeyuan <yubaoxiaolin@gmail.com>
2023-01-22 18:32:41 +0200

    setting up index view template

--- spring-boot-todo-application/src/main/java/com/wazooinc/springboottodoapplication/SpringBootTodoApplication.java
+++ spring-boot-todo-application/src/main/java/com/wazooinc/springboottodoapplication/SpringBootTodoApplication.java
@@ -3,11 +3,11 @@ package com.wazooinc.springboottodoapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="controllers")
 public class SpringBootTodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTodoApplication.class, args);
+		SpringApplication.run(SpringBootTodoApplication.class,args);
 	}
 
 }
